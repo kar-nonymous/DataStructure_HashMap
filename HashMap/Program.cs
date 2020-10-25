da8ebc85e1f2ba12a0002e4fb3926aad315ddaef
Kumar Kartikeya <kartikeya.dhoni@gmail.com>
2020-10-25 21:44:57 +0530

    [Kartikeya] UC added to find frequency

--- HashMap/Program.cs
+++ HashMap/Program.cs
@@ -9,18 +9,31 @@ namespace HashMap
             Console.WriteLine("Hello World!");
             MyMapNode<string, string> mapNode = new MyMapNode<string, string>(5);
             Console.WriteLine("Adding KeyValue pair");
-            mapNode.Add("0", "to");
-            mapNode.Add("1", "be");
-            mapNode.Add("2", "or");
-            mapNode.Add("3", "to");
-            mapNode.Add("4", "be");
-            mapNode.Add("5", "not");
+            mapNode.Add("0", "paranoids");
+            mapNode.Add("1", "are");
+            mapNode.Add("2", "not");
+            mapNode.Add("3", "paranoid");
+            mapNode.Add("4", "because");
+            mapNode.Add("5", "they");
+            mapNode.Add("5", "are");
+            mapNode.Add("5", "paranoid");
+            mapNode.Add("5", "but");
+            mapNode.Add("5", "because");
+            mapNode.Add("5", "they");
+            mapNode.Add("5", "keep");
+            mapNode.Add("5", "putting");
+            mapNode.Add("5", "themselves");
+            mapNode.Add("5", "deliberately");
+            mapNode.Add("5", "into");
+            mapNode.Add("5", "paranoid");
+            mapNode.Add("5", "avoidable");
+            mapNode.Add("5", "situations");
 
             Console.WriteLine("Displaying");
             mapNode.Display();
 
             Console.WriteLine("Frequency.....");
-            Console.WriteLine(mapNode.FindFrequency("to"));
+            Console.WriteLine(mapNode.FindFrequency("paranoid"));
         }
     }
 }
